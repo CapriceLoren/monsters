8e18b815b3d205a327ccd5d3aaeebf3b6df1b3bf
CapriceLoren <capriceloren@gmail.com>
2022-12-19 15:49:06 -0500

    implement search

--- monsters-rolo/src/App.js
+++ monsters-rolo/src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // using class component instead of functional
@@ -10,31 +9,42 @@ class App extends Component {
 
     this.state = {
       //always json object
-      monsters: [
-      {
-        name: 'Linda',
-        id: 1
-      },
-      {
-        name: 'Frank',
-        id: 2
-      },
-      {
-        name: 'Jacky',
-        id: 3
-        },
-      {
-        name: 'Caprice',
-        id: 4
-      }]
-    }
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    //first time component gets placed onto DOM, only happens once unless it has been unmounted
+    //this is when you want to make an API request
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      }
+      ))
   }
 
   render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
   return (
     <div className="App">
+      <input
+        className='search-box'
+        type='search'
+        placeholder='search monsters'
+        onChange={(event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(() => {
+          return {searchField}
+        })
+      }}/>
       {
-        this.state.monsters.map((monster) => {
+        filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>
           // key property stops error, usually id value as it will be completely unique. monster.name could be duplicated, and react would rerender both
           // add key to highest level (ie, if this h1 was inside of a div)
